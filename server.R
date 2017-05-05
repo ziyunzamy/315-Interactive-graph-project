92bc572f80c044758793956f74cbb6756c937539
Yunwen Cai <yunwencai@Yunwens-Air.wv.cc.cmu.edu>
2017-05-05 16:20:35 -0400

    rough draft

--- server.R
+++ server.R
@@ -1,8 +1,11 @@
 library(shiny)
 library(tidyverse)
+library(forcats)
 
 # https://www.kaggle.com/START-UMD/gtd
-terr = read.csv("~/Desktop/36-315/Interactive Graphic Project/terr.csv")
+
+terr.clean = read.csv("~/Desktop/36-315/315-Interactive-graph-project/terr.clean.csv")
+
 group13_315_theme <-  theme_bw() +  
   theme(
     plot.title = element_text(size = 16, color = "navy",face="bold"),
@@ -13,37 +16,119 @@ group13_315_theme <-  theme_bw() +
     panel.grid.minor = element_blank(),
     panel.background = element_rect(fill = "white")
   )
+my_colors <- c("#000000", "#56B4E9", "#E69F00", "#F0E442", "#009E73", "#0072B2", 
+               "#D55E00", "#CC7947")
 
 function(input, output) {
-  output$first_plot <- renderPlot({
-    p <- ggplot(data = survey, aes(x = Happiness.in.life)) + 
-      geom_bar(aes(fill = Gender)) +
-      labs(title = "Happiness in Life", x = "Scale") +
-      group13_315_theme
-    print(p)
-  })
   
-  output$second_plot <- renderPlot({
-    p <- ggplot(data = terr, aes(x = iyear)) + 
+  output$first_plot <- renderPlot({
+    p1 <- ggplot(data = terr.clean, aes(x = iyear)) + 
       geom_histogram(aes(y=..density..), bins = as.numeric(input$n_breaks),
                      color = "black", fill="pink") +
-      labs(title = "Age of Young People", x = "Age", y = "Density")+
+      labs(title = "Distribution of Terrorist Attacks",
+           x = "Year of Attacks", y = "Density")+
       group13_315_theme
     
     if (input$show_mean) {
-      p <- p + geom_vline(aes(xintercept = mean(iyear, na.rm = TRUE)),
+      p1 <- p1 + geom_vline(aes(xintercept = mean(iyear, na.rm = TRUE)),
                           color = "blue", linetype = "dashed", size = 1.5)
     }
     
     if (input$show_median) {
-      p <- p + geom_vline(aes(xintercept = median(iyear, na.rm = TRUE)),
+      p1 <- p1 + geom_vline(aes(xintercept = median(iyear, na.rm = TRUE)),
                           color = "yellow", linetype = "dashed", size = 1.5)
     }
     
     if (input$density) {
-      p <- p + geom_density(colour="red",
+      p1 <- p1 + geom_density(colour="red",
                             adjust=input$bw_adjust, size = 1)
     }
-    print(p)
+    print(p1)
   })
+  
+  output$second_plot <- renderPlot({
+    p2 <- ggplot(data = terr.clean, aes(x = nkill, y = nwound)) + 
+      geom_point(aes(color = doubtterr), size = input$pointSize) + 
+      labs(x = "Number of Deaths",
+           y = "Number of Wounds", color = "Is Terrorism") + 
+      ggtitle("Deaths vs Wounds") +
+      group13_315_theme
+    
+    if (input$smoothLine) {
+      if (input$se) {
+        p2 <- p2 + geom_smooth()
+      }
+      else p2 <- p2 + geom_smooth(se = FALSE)
+    }
+
+    print(p2)
+  })
+  
+  output$third_plot <- renderPlot({
+    p3 <- ggplot(data = terr.clean, aes(x = iyear)) + 
+      geom_histogram(aes(y=..density..), bins = as.numeric(input$n_breaks_3),
+                     color = "black", fill="pink") +
+      facet_grid(success~suicide) +
+      labs(title = "Distribution of Terrorist Attacks",
+           x = "Year of Attacks", y = "Density")+
+      group13_315_theme + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    
+    if (input$show_mean_3) {
+      p3 <- p3 + geom_vline(aes(xintercept = mean(iyear, na.rm = TRUE)),
+                            color = "blue", linetype = "dashed", size = 1.5)
+    }
+    
+    if (input$show_median_3) {
+      p3 <- p3 + geom_vline(aes(xintercept = median(iyear, na.rm = TRUE)),
+                            color = "yellow", linetype = "dashed", size = 1.5)
+    }
+    
+    if (input$density_3) {
+      p3 <- p3 + geom_density(colour="red",
+                              adjust=input$bw_adjust, size = 1)
+    }
+    print(p3)
+  })
+  
+  output$fourth_plot <- renderPlot({
+    p4 <- ggplot(data = terr.clean, aes(x = attacktype1_txt)) + 
+      geom_bar(color = "black", fill="pink") +
+      labs(title = "Distribution of Terrorist Attacks by Attack Type",
+           x = "Type of Attacks", y = "Number of Attacks") +
+      group13_315_theme + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    
+    if (input$show_num_4) {
+      p4 <- p4 + geom_text(stat = "count",
+                           aes(y = ..count.., label = ..count..), vjust = -.3)
+    }
+    print(p4)
+  })
+  
+  output$fifth_plot <- renderPlot({
+    p5 <- ggplot(data = terr.clean, aes(x = targtype1_txt)) + 
+      geom_bar(color = "black", fill="pink") +
+      labs(title = "Distribution of Terrorist Attacks by Target Type",
+           x = "Type of Targets", y = "Number of Attacks") +
+      group13_315_theme + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    
+    if (input$show_num_5) {
+      p5 <- p5 + geom_text(stat = "count",
+                           aes(y = ..count.., label = ..count..), vjust = -.3)
+    }
+    
+    print(p5)
+  })
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- ui.R
+++ ui.R
@@ -6,14 +6,18 @@ shinyUI(dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("PartA", tabName = "PartA", icon = icon("th")),
-      menuItem("PartB", tabName = "PartB", icon = icon("th"))
+      menuItem("PartB", tabName = "PartB", icon = icon("th")),
+      menuItem("PartC", tabName = "PartC", icon = icon("th"))
     )
   ),
   dashboardBody(
     tabItems(
       tabItem(tabName = "PartA",
-              fluidRow(
-                box(plotOutput(outputId = "second_plot", height = "250")),
+              tabsetPanel(
+                tabPanel("Plot1",
+                
+                fluidRow(
+                box(plotOutput(outputId = "first_plot", height = "250")),
                 
                 box(
                   title = "Controls",
@@ -22,10 +26,10 @@ shinyUI(dashboardPage(
                               choices = c(5,10,15),
                               selected = 15),
                   checkboxInput(inputId = "show_mean",
-                                label = strong("Show mean of age"),
+                                label = strong("Show mean of number of attacks"),
                                 value = FALSE),
                   checkboxInput(inputId = "show_median",
-                                label = strong("Show median of age"),
+                                label = strong("Show median of number of attack"),
                                 value = FALSE),
                   checkboxInput(inputId = "density",
                                 label = strong("Show density estimate"),
@@ -35,32 +39,85 @@ shinyUI(dashboardPage(
                   conditionalPanel(condition = "input.density == true",
                                    sliderInput(inputId = "bw_adjust",
                                                label = "Bandwidth adjustment:",
-                                               min = 0.2, max = 2, value = 1.2, step = 0.2))
+                                               min = 0.2, max = 2, value = 1, step = 0.2))
                 )
               )),
+                tabPanel("Plot2",
+                         
+                         fluidRow(
+                           box(plotOutput(outputId = "third_plot", height = "250")),
+                           
+                           box(
+                             title = "Controls",
+                             selectInput(inputId = "n_breaks_3",
+                                         label = "Number of bins in the histogram",
+                                         choices = c(5,10,15),
+                                         selected = 15),
+                             checkboxInput(inputId = "show_mean_3",
+                                           label = strong("Show mean of number of attacks"),
+                                           value = FALSE),
+                             checkboxInput(inputId = "show_median_3",
+                                           label = strong("Show median of number of attack"),
+                                           value = FALSE),
+                             checkboxInput(inputId = "density_3",
+                                           label = strong("Show density estimate"),
+                                           value = FALSE),
+                             
+                             # Display this only if the density is shown
+                             conditionalPanel(condition = "input.density_3 == true",
+                                              sliderInput(inputId = "bw_adjust",
+                                                          label = "Bandwidth adjustment:",
+                                                          min = 0.2, max = 2, value = 1, step = 0.2))
+                           )
+                         ))
+              
+              
+              )),
+              
       tabItem(tabName = "PartB", 
               tabsetPanel(
                 tabPanel("Plot1", 
                          fluidRow(
-                           box(plotOutput(outputId = "first_plot", height = "300px")),
+                           box(plotOutput(outputId = "second_plot", height = "300px")),
                            box(
                              title = "Controls",
-                             checkboxInput(inputId = "smooth.line",
+                             checkboxInput(inputId = "smoothLine",
                                            label = strong("Show Fitted Line"),
                                            value = FALSE),
-                             sliderInput(inputId = "point.size",
-                                         label = "point size:",
-                                         min = 1, max = 10, value = 5, step = 1)
+                             conditionalPanel(condition = "input.smoothLine == true",
+                                              checkboxInput(inputId = "se",
+                                              label = strong("Show Standard Error of Fitted Line"),
+                                              value = FALSE)),
+                             sliderInput(inputId = "pointSize",
+                                         label = strong("Point Size"),
+                                         min = 0.1, max = 2, value = 1, step = 0.1)
                            )
-                         )),
-                tabPanel("Plot2", fluidRow(
-                  box(plotOutput(outputId = "countour", height = "300px")),
-                  box(
-                    title = "Controls",
-                    checkboxInput(inputId = "points.visible",
-                                  label = strong("Show Points"),
-                                  value = FALSE))
-                ))
+                         ))
+              )
+      ),
+      
+      tabItem(tabName = "PartC", 
+              tabsetPanel(
+                tabPanel("Plot1", 
+                         fluidRow(
+                           box(plotOutput(outputId = "fourth_plot",
+                                          height = "300px")),
+                           box(
+                             title = "Controls",
+                             checkboxInput(inputId = "show_num_4",
+                                           label = strong("Show Number of Attacks"),
+                                           value = FALSE)
+                         ))),
+                tabPanel("Plot2", 
+                         fluidRow(
+                           box(plotOutput(outputId = "fifth_plot",
+                                          height = "300px")),
+                           box(
+                             title = "Controls",
+                             checkboxInput(inputId = "show_num_5",
+                                           label = strong("Show Number of Attacks"),
+                                           value = FALSE)
+                           )))
               )
       )
     )
